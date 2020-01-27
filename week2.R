d37baf99d9a36a6bb796d45d14f41db26fab861d
quxxx179 <quxxx179@MacBook-Pro-5.local>
2020-01-28 00:21:57 +0800

    commit for week2 hw

--- week2.R
+++ week2.R
@@ -1,3 +1,12 @@
 #Import and Labeling
 rt_df <- read.csv("~/Project2/data/week2.csv", header = TRUE)
 rt_df$condition <- factor(rt_df$condition, levels = c("A", "B"), labels = c("Control", "Experimental"))
+rt_df$gender <- factor(rt_df$gender, levels = c("F", "M", "T"), labels = c("Male", "Female", "Transgender"))
+
+#Analysis
+mean(rt_df$rt)
+rt_f_df <- subset(rt_df, gender == "Female")
+summary(rt_f_df)
+hist(rt_f_df$rt)
+datasets <- list(rt_df, rt_f_df)
+datasets[[1]][,2]
